839a99737120eaab91de7c30fc6d72025ebf1301
Mickey <kyle_mickey@trimble.com>
2023-01-09 16:30:27 -0700

    Running vinyl_api

---
+++ vinyl_api/README.md
@@ -0,0 +1,3 @@
+# vinyl_api
+
+This API is used to extend the functionality of our low-code application. It is run using `uvicorn` and `fastapi`.
---
+++ vinyl_api/src/main.py
@@ -0,0 +1,12 @@
+'''
+FastAPI entrypoint
+'''
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+async def root():
+    return {'message': 'Works!'}
